dba0e82cb01e562be4cf21f2895b4b9f8054dfe0
Andres Amaya <andresrobertoamayabracamonte@gmail.com>
2024-12-05 13:09:40 -0700

    inicializacion de proyecto

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='datos'>
+        <label>Nombre: <input type='text'></input></label>
+        <label>Edad: <input type='text'></input></label>
+        <label>Pais: <input type='text'></input></label>
+        <label>Cargo: <input type='text'></input></label>
+        <label>Años: <input type='text'></input></label>
+        <label>Nombre: <input type='text'></input></label>
+      </div>
+    </div>
+  );
+}
+
+export default App;
